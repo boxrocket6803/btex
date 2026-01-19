c2dfa5160b45f1bbadaeef0b8f70bac99d9d247b
boxrocket <boxrocket6803@gmail.com>
2026-01-18 21:39:40 -0600

    openexr to btex conversion

---
+++ Convert.OpenEXR.cs
@@ -0,0 +1,38 @@
+﻿using TinyEXR;
+
+[Convert.Association(["exr"])]
+public class OpenEXR : Convert.Source {
+	private float[] Data;
+
+	public override void Read(string path) {
+		Exr.LoadEXR(path, out Data, out var w, out var h);
+		Width = (ushort)w;
+		Height = (ushort)h;
+	}
+	public override byte[] LoadByte(int channels) {
+		var load = new byte[Width * Height * channels];
+		var loadw = 0;
+		var ch = 0;
+		foreach (var pixel in Data) {
+			if (ch < channels)
+				load[loadw++] = (byte)(pixel * byte.MaxValue);
+			ch++;
+			if (ch > 3)
+				ch = 0;
+		}
+		return load;
+	}
+	public override uint[] LoadUint(int channels) {
+		var load = new uint[Width * Height * channels];
+		var loadw = 0;
+		var ch = 0;
+		foreach (var pixel in Data) {
+			if (ch < channels)
+				load[loadw++] = (uint)(pixel * uint.MaxValue);
+			ch++;
+			if (ch > 3)
+				ch = 0;
+		}
+		return load;
+	}
+}
---
+++ Texture.cs
@@ -0,0 +1,12 @@
+﻿public partial class Texture {
+	public enum Format {
+		rgba8888 = 0,
+		rgb888 = 1,
+		g8 = 2,
+		g32 = 3,
+	}
+
+	public uint Width;
+	public uint Height;
+	public uint Depth;
+}
